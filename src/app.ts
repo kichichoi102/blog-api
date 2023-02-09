495a6941e5248ebe46357d425546481f76619390
kichichoi102@gmail.com <kichichoi102@gmail.com>
2023-02-09 12:10:24 -0500

    linter doesnt work????

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,13 @@ import express from "express";
 
 async function startServer() {
     const app = express();
+
+    app.listen(4000, () => {
+        console.log('Listening on port: 4000');
+    }).on('error', err => {
+        console.log(err);
+        process.exit(1);
+    });
 }
 
 startServer()
