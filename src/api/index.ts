b3cdda8ef0377ab62937b214c7c59b7568896330
kichichoi102@gmail.com <kichichoi102@gmail.com>
2023-02-11 11:46:46 -0500

    added all required get required endpoints

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,13 @@
-import { Router } from "express";
-import user from './routes/user'
-
+import { Router } from 'express';
+import user from './routes/user';
+import post from './routes/post';
+import comment from './routes/comment';
 
 export default () => {
-    const router = Router();
-    user(router);
+  const router = Router();
+  user(router);
+  post(router);
+  comment(router);
 
-    return router
-}
+  return router;
+};
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser'
 import routes from '@/api';
 import config from '@/config';
 
@@ -11,21 +12,29 @@ export default ({ app }: { app: express.Application }) => {
     res.status(200).end();
   });
 
+  app.use(bodyParser.json())
+
   // jsonify req.body-> json
   app.use(express.json());
   // load api routes
   app.use(config.api.prefix, routes());
 
   // expected common error handling
-  app.use((req, res, next) => {
+  // Endpoint not found handler
+  app.use((req: Request, res: Response, next: NextFunction) => {
     const err = new Error('Not Found');
     err['status'] = 404;
     next(err);
   });
-  app.use((err, req, res, next) => {
+  // Unauthorized Handler
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     if (err.name === 'UnauthorizedError') {
       return res.status(err.status).send({ message: err.message }).end();
     }
     return next(err);
   });
+  // Catch All server error handler
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500).json();
+});
 };
