948dec350d54b0bbe19ce3bdaa227b267223d7c4
kichichoi102@gmail.com <kichichoi102@gmail.com>
2023-02-12 13:00:25 -0500

    added dockerfile and postgres connection

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,13 @@
+import client from './postgres';
 import expressLoader from './express';
 import Logger from './logger';
 
 export default async ({ expressApp }) => {
+  await client.connect();
+
+  const res = await client.query('SELECT $1::text as message', ['Hello world!']);
+  console.log(res.rows[0].message);
+
   await expressLoader({ app: expressApp });
-  Logger.info('Express Loaded')
+  Logger.info('Express Loaded');
 };
