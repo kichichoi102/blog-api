30795d7939108bd4ae58ea6424aa66ef6c381428
kichichoi102@gmail.com <kichichoi102@gmail.com>
2023-02-12 21:35:59 -0500

    added docker compose to fetch postgres and adminer images. Also confirmed connection to db, and created initializer

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,12 +1,11 @@
-import client from './postgres';
+import client from './infrastructure/postgresClient';
+import initializer from './infrastructure/postgresInitializer';
 import expressLoader from './express';
 import Logger from './logger';
 
 export default async ({ expressApp }) => {
   await client.connect();
-
-  const res = await client.query('SELECT $1::text as message', ['Hello world!']);
-  console.log(res.rows[0].message);
+  await initializer(client);
 
   await expressLoader({ app: expressApp });
   Logger.info('Express Loaded');
